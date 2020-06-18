c75fbc6fe05ef5a32dcce28efd3741e84b9eee02
Rellinxe27 <rellinxe@gmail.com>
2020-06-18 12:36:13 +0100

    Initial commit

---
+++ src/Week01/W1dot1.java
@@ -0,0 +1,11 @@
+package Week01;
+
+public class W1dot1 {
+
+    public static void main(String[] args) {
+        System.out.println("Jeremiah Jones");
+        System.out.println("CIT260-07");
+        System.out.println("Lehi, Utah");
+        System.out.println("Banana Cream Pie");
+    }
+}
